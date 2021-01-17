3504060627357e6ad0a75311ed4f80724e34521c
Lukas Grünwald <gruenwald.lk@outlook.com>
2021-01-17 18:02:54 +0100

    Initial Release

---
+++ scripts/library/eaw-layerz/configuration.lua
@@ -0,0 +1,42 @@
+---@class Configuration
+Configuration = {
+    ---@type string
+    ---The neutral faction alias in capital letters.
+    NEUTRAL_PLAYER = "NEUTRAL",
+    ---@type table[]
+    ---A map of anonymous objects that define the mapping between the category mask and (a subset of) bones of the `LAYER_DUMMY_TYPE`.
+    CATEGORY_MAP = {
+        {
+            ---@type string
+            ID = "Corvette",
+            ---@return integer l_min minimum offset
+            ---@return integer l_max maximum offset
+            ---**TODO:** Update to desired range.
+            get_range = function() return 0, 0 end
+        },
+        {
+            -- @type string
+            ID = "Frigate",
+            ---@return integer l_min minimum offset
+            ---@return integer l_max maximum offset
+            ---**TODO:** Update to desired range.
+            get_range = function() return 0, 0 end
+
+        },
+        {
+            -- @type string
+            ID = "Capital",
+            ---@return integer l_min minimum offset
+            ---@return integer l_max maximum offset
+            ---**TODO:** Update to desired range.
+            get_range = function() return 0, 0 end
+
+        },
+        ---@return integer l_min minimum offset
+        ---@return integer l_max maximum offset
+        ---**TODO:** Update to desired range.
+        default = function() return 0, 0 end
+    }
+}
+
+return Configuration
---
+++ scripts/library/eaw-layerz/layermanager.lua
@@ -0,0 +1,107 @@
+---@class LayerManager
+---*NOTE:* Functions prefixed with a double underscore (`__`) are considered private functions of the module. Call at your own risk.
+LayerManager = {
+    ---@type Configuration
+    CONFIG = require("eaw-layerz/configuration"),
+    ---@type PlayerObject
+    m_actual_owner = nil,
+    ---@private
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    ---Hides a given `GameObject` reliably by calling `GameObjectWrapper::Hide(bool b)` twice.
+    __hide_game_object = function(self, game_object)
+        game_object.Hide(true)
+        game_object.Hide(true)
+    end,
+    ---@private
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    ---@param spawns_fighters boolean
+    ---Completely disables a given `GameObject`.
+    __disable_game_object = function(self, game_object, spawns_fighters)
+        if not game_object.Get_Owner().Is_Human() then
+            self:__disable_game_object_for_ai_player(game_object)
+        end
+        local l_spawns_fighters = spawns_fighters or false
+        if l_spawns_fighters then game_object.Set_Garrison_Spawn(false) end
+        game_object.Make_Invulnerable(true)
+        game_object.Set_Selectable(false)
+        game_object.Prevent_All_Fire(true)
+    end,
+    ---@private
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    ---Performs additional actions required for AI-owned `GameObjects`so they get added (back) to the `FreeStore` reliably.
+    __disable_game_object_for_ai_player = function(self, game_object)
+        game_object.Prevent_AI_Usage(true)
+        self.m_actual_owner = game_object.Get_Owner()
+        game_object.Change_Owner(Find_Player(
+                                     self.CONFIG.NEUTRAL_PLAYER or "NEUTRAL"))
+    end,
+    ---@private
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    ---@param spawns_fighters boolean
+    ---Completely re-enables a given `GameObject` that waas previously disabled by `LayerManager#__disable_game_object`
+    ---@see LayerManager#__disable_game_object
+    __enable_game_object = function(self, game_object, spawns_fighters)
+        local l_spawns_fighters = spawns_fighters or false
+        game_object.Make_Invulnerable(false)
+        game_object.Set_Selectable(true)
+        game_object.Prevent_All_Fire(false)
+        if not game_object.Get_Owner().Is_Human() then
+            self:__enable_game_object_for_ai_player(game_object)
+        end
+        if l_spawns_fighters then game_object.Set_Garrison_Spawn(true) end
+    end,
+    ---@private
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    ---Performs additional actions required for AI-owned `GameObjects`so they get added (back) to the `FreeStore` reliably.
+    __enable_game_object_for_ai_player = function(self, game_object)
+        game_object.Change_Owner(self.m_actual_owner)
+        game_object.Prevent_AI_Usage(false)
+    end,
+    ---@private
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    __randomise_z_axis_coordinate = function(self, game_object)
+        local l_position = game_object.Get_Position()
+        local l_min, l_max = self:__resolve_layer(game_object)
+        if l_min == l_max then return end
+        local l_x0, l_x1, l_x2 = l_position.Get_XYZ()
+        l_position =
+            Create_Position(l_x0, l_x1, l_x2 + GameRandom(l_min, l_max))
+        game_object.Teleport(l_position)
+    end,
+    ---@private
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    ---@return integer minimum
+    ---@return integer maximum
+    ---Returns the possible bones.
+    __resolve_layer = function(self, game_object)
+        for _, l_category_map in pairs(self.CONFIG.CATEGORY_MAP) do
+            if game_object.Is_Category(l_category_map.ID) then
+                return l_category_map.get_range()
+            end
+        end
+        return self.CONFIG.CATEGORY_MAP.default()
+    end,
+    ---@param self LayerManager
+    ---@param game_object GameObject
+    ---@param spawns_fighters boolean
+    ---Moves the units to a randomised Z-coordinate based on its category masks and the configurations made.
+    ---@see Configuration
+    update_unit_layer = function(self, game_object, spawns_fighters)
+        if not spawns_fighters then spawns_fighters = false end
+        self:__disable_game_object(game_object, spawns_fighters)
+        game_object.Cancel_Hyperspace()
+        self:__hide_game_object(game_object)
+        self:__randomise_z_axis_coordinate(game_object)
+        game_object.Cinematic_Hyperspace_In(1)
+        self:__enable_game_object(game_object, spawns_fighters)
+    end
+}
+
+return LayerManager
